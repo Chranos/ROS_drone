8eb52a3a21af5bcb3245ec7b45f5bbcc2d38cce3
Chranos <826995883@qq.com>
2022-10-24 11:03:33 +0800

    camera

---
+++ src/usb_cam/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/chranos/ROS/ROS_drone/src/usb_cam
+# Build directory: /home/chranos/ROS/ROS_drone/src/usb_cam/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
